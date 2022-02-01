8296a0f3785f1d297f41f150c914cf5c6017279f
yzamir <yzamir@redhat.com>
2022-02-01 09:35:15 +0200

    Add models

---
+++ console/models/NetworkAttachmentDefinitionModel.ts
@@ -0,0 +1,21 @@
+import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+import { modelToGroupVersionKind, modelToRef } from '../modelUtils'; 
+
+const NetworkAttachmentDefinitionModel: K8sModel = {
+  label: 'Network Attachment Definition',
+  labelPlural: 'Network Attachment Definitions',
+  apiVersion: 'v1',
+  apiGroup: 'k8s.cni.cncf.io',
+  plural: 'network-attachment-definitions',
+  namespaced: true,
+  abbr: 'NAD',
+  kind: 'NetworkAttachmentDefinition',
+  id: 'network-attachment-definition',
+  crd: true,
+  legacyPluralURL: true,
+};
+
+export const NetworkAttachmentDefinitionModelGroupVersionKind = modelToGroupVersionKind(NetworkAttachmentDefinitionModel);
+export const NetworkAttachmentDefinitionModelRef = modelToRef(NetworkAttachmentDefinitionModel);
+
+export default NetworkAttachmentDefinitionModel;
---
+++ console/models/index.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './VirtualMachineModel';
+export * from './VirtualMachineInstanceModel';
+export * from './VirtualMachineSnapshotModel';
+export * from './VirtualMachineRestoreModel';
+export * from './KubeDeschedulerModel';
+
+export * from './DataVolumeModel';
+export * from './DataSourceModel';
+export * from './CDIConfigModel';
+export * from './StorageProfileModel';
+
+export * from './NetworkAttachmentDefinitionModel';
+export * from './NetworkAddonsConfigModel';
+export * from './SriovNetworkNodePolicyModel';
+
+export * from './HyperConvergedModel';
+export * from './SubscriptionModel';
+export * from './PackageManifestModel';

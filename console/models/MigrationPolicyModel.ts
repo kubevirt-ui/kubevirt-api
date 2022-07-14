1c41837f10a160aa91821af6b56fdeb655037b90
Aviv Turgeman <aturgema@redhat.com>
2022-07-14 16:15:40 +0300

    Add MigrationPolicyModel and VirtualMachineInstanceMigrationModel (#23)

---
+++ console/models/MigrationPolicyModel.ts
@@ -0,0 +1,24 @@
+import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+
+import { modelToGroupVersionKind, modelToRef } from '../modelUtils';
+
+const MigrationPolicyModel: K8sModel = {
+  label: 'MigrationPolicy',
+  labelKey: 'MigrationPolicy',
+  labelPlural: 'MigrationPolicies',
+  labelPluralKey: 'MigrationPolicies',
+  apiVersion: 'v1alpha1',
+  apiGroup: 'migrations.kubevirt.io',
+  plural: 'migrationpolicies',
+  abbr: 'MP',
+  namespaced: false,
+  kind: 'MigrationPolicy',
+  id: 'migrationpolicy',
+  crd: true,
+};
+
+export const MigrationPolicyModelGroupVersionKind =
+    modelToGroupVersionKind(MigrationPolicyModel);
+export const MigrationPolicyModelRef = modelToRef(MigrationPolicyModel);
+
+export default MigrationPolicyModel;
---
+++ console/models/VirtualMachineInstanceMigrationModel.ts
@@ -0,0 +1,22 @@
+import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+
+import { modelToGroupVersionKind, modelToRef } from '../modelUtils';
+
+const VirtualMachineInstanceMigrationModel: K8sModel = {
+    label: 'VirtualMachineInstanceMigration',
+    labelPlural: 'VirtualMachineInstanceMigrations',
+    apiVersion: 'v1',
+    apiGroup: 'kubevirt.io',
+    plural: 'virtualmachineinstancemigrations',
+    abbr: 'VMIM',
+    namespaced: true,
+    kind: 'VirtualMachineInstanceMigration',
+    id: 'virtualmachineinstancemigration',
+    crd: true,
+};
+
+export const VirtualMachineInstanceMigrationModelGroupVersionKind =
+    modelToGroupVersionKind(VirtualMachineInstanceMigrationModel);
+export const VirtualMachineInstanceMigrationModelRef = modelToRef(VirtualMachineInstanceMigrationModel);
+
+export default VirtualMachineInstanceMigrationModel;

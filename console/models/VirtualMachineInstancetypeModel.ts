0860590416d8e1afccfbc5aabbe2cc8193582984
Ugo Palatucci <upalatuc@redhat.com>
2023-03-15 09:28:21 +0100

    feat: add vm instancetype and preferences non-cluster wide

---
+++ console/models/VirtualMachineInstancetypeModel.ts
@@ -0,0 +1,23 @@
+import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+
+import { modelToGroupVersionKind, modelToRef } from '../modelUtils';
+
+const VirtualMachineInstancetypeModel: K8sModel = {
+  label: 'VirtualMachineInstancetype',
+  labelPlural: 'VirtualMachineInstancetypes',
+  apiVersion: 'v1alpha2',
+  apiGroup: 'instancetype.kubevirt.io',
+  plural: 'virtualmachineinstancetypes',
+  abbr: 'VMCI',
+  namespaced: false,
+  kind: 'VirtualMachineInstancetype',
+  id: 'virtualmachineinstancetype',
+  crd: true,
+};
+
+export const VirtualMachineInstancetypeModelGroupVersionKind = modelToGroupVersionKind(
+  VirtualMachineInstancetypeModel,
+);
+export const VirtualMachineInstancetypeModelRef = modelToRef(VirtualMachineInstancetypeModel);
+
+export default VirtualMachineInstancetypeModel;

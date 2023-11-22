8e261e8c4d035c440230e84d7821421e0b896c7c
Ugo Palatucci <upalatuc@redhat.com>
2023-11-22 10:10:29 +0100

    Add VirtualMachineClone model

---
+++ console/models/VirtualMachineCloneModel.ts
@@ -0,0 +1,22 @@
+import { K8sModel } from '@openshift-console/dynamic-plugin-sdk/lib/api/common-types';
+
+import { modelToGroupVersionKind, modelToRef } from '../modelUtils';
+
+const VirtualMachineCloneModel: K8sModel = {
+  label: 'VirtualMachineClone',
+  labelPlural: 'VirtualMachineClones',
+  apiVersion: 'v1alpha1',
+  apiGroup: 'clone.kubevirt.io',
+  plural: 'virtualmachineclones',
+  abbr: 'VMS',
+  namespaced: true,
+  kind: 'VirtualMachineClone',
+  id: 'virtualmachineclone',
+  crd: true,
+};
+
+export const VirtualMachineCloneModelGroupVersionKind =
+  modelToGroupVersionKind(VirtualMachineCloneModel);
+export const VirtualMachineCloneModelRef = modelToRef(VirtualMachineCloneModel);
+
+export default VirtualMachineCloneModel;
